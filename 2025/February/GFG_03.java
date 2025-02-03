b373944c4413d9ad7fac06d6922268b09eb420e2
Badass-Nemesis <harshitanant29@gmail.com>
2025-02-03 14:58:54 +0530

    I think the potd gods are going easy on me, giving easy questions for the past 3 days

---
+++ 2025/February/GFG_03.java
@@ -0,0 +1,39 @@
+// Qes -> https://www.geeksforgeeks.org/problems/height-of-binary-tree/1
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class Solution {
+    public static int dfs(Node node, int count) {
+        if (node.left == null && node.right == null) {
+            return count;
+        }
+
+        int tempCount = 0;
+
+        if (node.left != null) {
+            tempCount = Math.max(dfs(node.left, count + 1), tempCount);
+        }
+
+        if (node.right != null) {
+            tempCount = Math.max(dfs(node.right, count + 1), tempCount);
+        }
+
+        return tempCount;
+    }
+
+    // Function to find the height of a binary tree.
+    int height(Node node) {
+        // code here
+        return dfs(node, 0);
+    }
+}
---
+++ 2025/February/LC_03.java
@@ -0,0 +1,71 @@
+// Qes ->
+// https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description/
+
+/* ------- THIS IS MY DUMB SOLUTION ---------- */
+
+// class Solution {
+// public int longestMonotonicSubarray(int[] nums) {
+// if (nums.length == 1) {
+// return 1;
+// }
+
+// int maxCount = 0;
+
+// for (int i = 1; i < nums.length; i++) {
+// int tempCount = 0;
+
+// if (nums[i - 1] < nums[i]) {
+// // increasing case
+// while (i < nums.length && nums[i - 1] < nums[i]) {
+// tempCount++;
+// i++;
+// }
+// maxCount = Math.max(tempCount, maxCount);
+// } else if (nums[i - 1] > nums[i]) {
+// // decreasing case
+// while (i < nums.length && nums[i - 1] > nums[i]) {
+// tempCount++;
+// i++;
+// }
+// maxCount = Math.max(tempCount, maxCount);
+// }
+
+// if (tempCount > 0) {
+// i--;
+// }
+// }
+
+// return maxCount + 1;
+// }
+// }
+
+/* -------- IMPROVED SOLUTION --------- */
+
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
+
+        int maxCount = 0;
+        int increasingCount = 0;
+        int decreasingCount = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                increasingCount++;
+                decreasingCount = 0;
+            } else if (nums[i - 1] > nums[i]) {
+                decreasingCount++;
+                increasingCount = 0;
+            } else {
+                increasingCount = 0;
+                decreasingCount = 0;
+            }
+
+            maxCount = Math.max(maxCount, Math.max(increasingCount, decreasingCount));
+        }
+
+        return maxCount + 1; // adding 1 because default case is 1, but I took it 0
+    }
+}
